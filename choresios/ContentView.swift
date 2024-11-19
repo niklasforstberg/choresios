892a8398ba8944a7dee7fd07aac0360decdccc5f
niklasforstberg <github@forstberg.com>
2024-11-19 18:48:50 +0100

    First commit

---
+++ choresios/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  choresios
+//
+//  Created by Niklas Förstberg on 2024-11-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ choresios/choresiosApp.swift
@@ -0,0 +1,17 @@
+//
+//  choresiosApp.swift
+//  choresios
+//
+//  Created by Niklas Förstberg on 2024-11-19.
+//
+
+import SwiftUI
+
+@main
+struct choresiosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
